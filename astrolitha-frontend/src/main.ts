9617317fb4c062767ffb5aaaac08037e683308e0
Myazusa <myadzusa@icloud.com>
2025-05-09 16:49:59 +0900

    update

---
+++ astrolitha-frontend/src/main.ts
@@ -0,0 +1,11 @@
+﻿import {createApp} from "vue";
+import App from "@/App.vue";
+import router from "@/router/router";
+import ElementPlus from "element-plus"
+import {createPinia} from "pinia";
+
+createApp(App)
+    .use(router)
+    .use(createPinia())
+    .use(ElementPlus)
+    .mount('#app')
---
+++ astrolitha-frontend/src/router/router.ts
@@ -0,0 +1,30 @@
+﻿import {createRouter, createWebHistory, RouteRecordRaw} from "vue-router";
+
+const routes: RouteRecordRaw[] = [
+    {
+        path: '/',
+        component: ()=>import("@/App.vue"),
+        children:[
+            {
+                path:'/',
+                component: ()=>import("@/pages/UserLogin.vue"),
+            }
+        ]
+    }
+]
+
+const router = createRouter({
+    end: undefined, sensitive: undefined, strict: undefined,
+    history: createWebHistory('/'),
+    routes,
+    scrollBehavior(to){
+        if(to.hash){
+            return{
+                el:to.hash,
+                behavior: 'smooth', /*平滑滚动到标签*/
+            };
+        };
+    }
+});
+
+export default router;
