6fccf8e0c42a6a82d07e5566b6d6bc17e089fafe
Myazusa <myadzusa@icloud.com>
2025-06-10 09:38:43 +0900

    update

--- AstrolithaBackend/src/main/java/com/github/myazusa/astrolithabackend/service/agent/CustomAgentBuilderService.java
+++ AstrolithaBackend/src/main/java/com/github/myazusa/astrolithabackend/service/agent/CustomAgentBuilderService.java
@@ -3,12 +3,14 @@ package com.github.myazusa.astrolithabackend.service.agent;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.myazusa.astrolithabackend.common.exception.FileOperationException;
 import com.github.myazusa.astrolithabackend.common.exception.InjectException;
+import com.github.myazusa.astrolithabackend.common.inject.ToolMethod;
 import com.github.myazusa.astrolithabackend.common.inject.ToolMethodDelegate;
 import com.github.myazusa.astrolithabackend.dto.CustomToolFunctionRequestDTO;
 import lombok.Getter;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.dynamic.DynamicType;
 import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
+import net.bytebuddy.implementation.MethodCall;
 import net.bytebuddy.implementation.MethodDelegation;
 import org.springframework.ai.tool.ToolCallback;
 import org.springframework.ai.tool.definition.ToolDefinition;
@@ -34,6 +36,7 @@ public class CustomAgentBuilderService {
     private final ObjectMapper objectMapper = new ObjectMapper();
     public final static String CLASS_PACKAGE = "com.github.myazusa.astrolithabackend.service.gen.";
     private static final List<Map<String, Class<?>>> customMethodList = new ArrayList<>();
+
     @Getter
     private final List<ToolCallback> globalToolCallbacks = new ArrayList<>();
 
@@ -46,12 +49,21 @@ public class CustomAgentBuilderService {
         globalToolCallbacks.add(toolCallback);
     }
 
+    /**
+     * 构造工具并加入工具链
+     * @param customToolFunctionRequestDTO
+     */
     public void buildAgent(CustomToolFunctionRequestDTO customToolFunctionRequestDTO) {
         ToolCallback toolCallback = injectCustomToolCallMethod(customToolFunctionRequestDTO);
         addAgent(toolCallback);
     }
 
-    public ToolCallback injectCustomToolCallMethod(CustomToolFunctionRequestDTO customToolFunctionRequestDTO){
+    /**
+     * 从注入方法创建toolcall
+     * @param customToolFunctionRequestDTO 前端传过来的方法定义
+     * @return 工具
+     */
+    private ToolCallback injectCustomToolCallMethod(CustomToolFunctionRequestDTO customToolFunctionRequestDTO){
         Method method = constructAndRegisterMethod(customToolFunctionRequestDTO);
         ToolDefinition toolDefinition = ToolDefinitions.builder(method)
                 .name(customToolFunctionRequestDTO.getFunctionName())
@@ -63,17 +75,25 @@ public class CustomAgentBuilderService {
                 .build();
     }
 
+    /**
+     * 把前端传过来的对象定义做成方法
+     * @param customToolFunctionRequestDTO 前端传过来的方法定义
+     * @return 动态创建的方法
+     */
     private Method constructAndRegisterMethod(CustomToolFunctionRequestDTO customToolFunctionRequestDTO){
+        Method method = null;
+        try {
+            method = ToolMethod.class.getMethod("invoke", String.class, String.class, Object[].class);
+        } catch (NoSuchMethodException e) {
+            throw new InjectException("方法模板创建失败" + e.getMessage());
+        }
+
         try(DynamicType.Unloaded<Object> unloaded = new ByteBuddy()
                 .subclass(Object.class)
                 .name(CLASS_PACKAGE + customToolFunctionRequestDTO.getFunctionName())
                 .defineMethod(customToolFunctionRequestDTO.getFunctionName(), String.class, Modifier.PUBLIC)
-                .intercept(MethodDelegation.to(
-                                new ToolMethodDelegate(
-                                        customToolFunctionRequestDTO.getRemoteApi(),
-                                        customToolFunctionRequestDTO.getRequestMethod(),
-                                        customToolFunctionRequestDTO.getParams())
-                        )
+                .intercept(MethodCall.invoke(method)
+                        .with(customToolFunctionRequestDTO.getRemoteApi(), customToolFunctionRequestDTO.getRequestMethod(), customToolFunctionRequestDTO.getParams())
                 )
                 .make()) {
             Class<?> customClass = unloaded
@@ -82,12 +102,16 @@ public class CustomAgentBuilderService {
             Map<String, Class<?>> hashMap = new ConcurrentHashMap<>();
             hashMap.put(CLASS_PACKAGE + customToolFunctionRequestDTO.getFunctionName(),customClass);
             customMethodList.add(hashMap);
-            return customClass.getMethod(CLASS_PACKAGE + customToolFunctionRequestDTO.getFunctionName());
+            return customClass.getMethod(customToolFunctionRequestDTO.getFunctionName());
         } catch (NoSuchMethodException e) {
             throw new InjectException(e.getMessage());
         }
     }
 
+    /**
+     * 序列化保存方法定义，供下次启动创建
+     * @param customToolFunctionRequestDTO 前端传过来的方法定义
+     */
     private void serializationFunction(CustomToolFunctionRequestDTO customToolFunctionRequestDTO) {
         if (!Files.exists(functionDir)) {
             try {
